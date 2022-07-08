6dfb3b15cb2536aa5f58eb0e845915cdeaa1998b
keanallen <keanallenn@gmail.com>
2022-07-08 14:22:37 +0800

    wrap up

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -3,7 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:k_todo/blocs/todos/todos_bloc.dart';
 import 'package:k_todo/screens/add_todo_screen.dart';
 
-import '../models/todo_model.dart';
+import 'widgets/todo_item.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -12,6 +12,7 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     // AppBar
     var appBar = AppBar(
+      title: const Text("To Dos"),
       actions: [
         IconButton(
           onPressed: () => Navigator.push(
@@ -39,16 +40,19 @@ class HomePage extends StatelessWidget {
             child: CircularProgressIndicator(),
           );
         } else if (state is TodosLoaded) {
-          return Container(
-            padding: const EdgeInsets.all(8.0),
+          return SizedBox(
             width: double.infinity,
+            height: MediaQuery.of(context).size.height,
             child: Column(
               children: [
-                Container(
-                  padding: const EdgeInsets.all(8),
-                  child: const Text("Tasks"),
-                ),
-                if (state.todos.isEmpty) const Text("No To Do's found!"),
+                // No Todos
+                if (state.todos.isEmpty)
+                  const Expanded(
+                    child: Center(
+                      child: Text("No To Do's found!"),
+                    ),
+                  ),
+                // Show Todos
                 if (state.todos.isNotEmpty)
                   Expanded(
                     child: ListView.separated(
@@ -67,83 +71,6 @@ class HomePage extends StatelessWidget {
           return const Text("Something went wrong.");
         }
       },
-    );
-  }
-}
-
-class TodoItem extends StatelessWidget {
-  final Todo todo;
-  const TodoItem({
-    Key? key,
-    required this.todo,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return ListTile(
-      onLongPress: () {
-        showBottomSheet(
-            context: context,
-            builder: (context) {
-              return BlocListener<TodosBloc, TodosState>(
-                listener: (context, state) {
-                  if (state is TodosLoaded) {
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      const SnackBar(
-                        content: Text("Task Deleted"),
-                      ),
-                    );
-                  }
-                },
-                child: Container(
-                  color: Colors.white,
-                  padding: const EdgeInsets.all(8),
-                  height: 100,
-                  child: Column(
-                    children: [
-                      ListTile(
-                        leading: const Icon(Icons.delete),
-                        title: const Text("Delete"),
-                        onTap: () {
-                          context.read<TodosBloc>().add(DeleteTodo(todo: todo));
-                          Navigator.pop(context);
-                        },
-                      ),
-                    ],
-                  ),
-                ),
-              );
-            });
-      },
-      leading: FittedBox(
-          child: Text(
-        "#${todo.id}",
-        style: Theme.of(context).textTheme.headline6,
-      )),
-      title: Text(
-        "${todo.title}",
-        style: TextStyle(
-          decoration: todo.isCompleted ? TextDecoration.lineThrough : null,
-        ),
-      ),
-      subtitle: Text(
-        "${todo.description}",
-        style: TextStyle(
-          decoration: todo.isCompleted ? TextDecoration.lineThrough : null,
-        ),
-      ),
-      trailing: Checkbox(
-        value: todo.isCompleted,
-        onChanged: (val) {
-          context.read<TodosBloc>().add(
-                UpdateTodo(
-                  todo: todo.copyWith(
-                    isCompleted: !todo.isCompleted,
-                  ),
-                ),
-              );
-        },
-      ),
     );
   }
 }
